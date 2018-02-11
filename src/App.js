f7d5b5e9c8a096e1c797b8a13ae937eb8acf4621
EAGLE <apowerz007@hotmail.com>
2018-02-11 06:51:52 -0500

    General commit to add new files

--- src/App.js
+++ src/App.js
@@ -1,21 +1,46 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import ListContacts from './ListContacts'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    state = {
+        contacts: [
+            {
+                "id": "ryan",
+                "name": "Ryan Florence",
+                "email": "ryan@reacttraining.com",
+                "avatarURL": "http://localhost:5001/ryan.jpg"
+            },
+            {
+                "id": "michael",
+                "name": "Michael Jackson",
+                "email": "michael@reacttraining.com",
+                "avatarURL": "http://localhost:5001/michael.jpg"
+            },
+            {
+                "id": "tyler",
+                "name": "Tyler McGinnis",
+                "email": "tyler@reacttraining.com",
+                "avatarURL": "http://localhost:5001/tyler.jpg"
+            }
+        ]
+    }
+
+    removeContact = (contact) => {
+        this.setState((state) => ({
+            contacts: state.contacts.filter((c) => c.id !== contact.id)
+        }))
+    }
+
+    render() {
+        return (
+            <div>
+                <ListContacts
+                    onDeleteContact={this.removeContact}
+                    contacts={this.state.contacts}
+                />
+            </div>
+        );
+    }
 }
 
 export default App;
